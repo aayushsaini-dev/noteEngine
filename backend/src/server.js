f48e5f4ff31eaa2a6e110d7ea9c6b7b6a8d43eaf
Aayush Saini <aayushsaini0406@gmail.com>
2025-11-20 22:02:37 +0530

    deployment ready

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,30 +1,39 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import notesRoutes from "./routes/notesRoute.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-connectDB();
 // middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
 app.use(rateLimiter);
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method} & req url is ${req.url}`);
-//   next();
-// });
 
 // routes
 app.use("/api/notes", notesRoutes);
 
-app.listen(5001, () => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+connectDB();
+app.listen(PORT, () => {
   console.log("Server is running on port 5001");
 });
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001/api"
+  : "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
